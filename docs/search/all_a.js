238f7b770cb85d510cea3b4e4ca7797b74eeec6f
mbrzecki <maciej.brzecki@gmail.com>
2018-09-05 23:24:25 +0200

    Correcting documentation

--- docs/search/all_a.js
+++ docs/search/all_a.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['junified_20library_20integrating_20analytical_20and_20numerical_20techiniques',['jUnified Library Integrating Analytical and Numerical Techiniques',['../index.html',1,'']]],
+  ['junified_20library_20integrating_20analytical_20and_20numerical_20techniques',['jUnified Library Integrating Analytical and Numerical Techniques',['../index.html',1,'']]],
   ['jpyholiday',['JPYHoliday',['../a00138.html',1,'julian']]],
   ['jpyholiday_2ehpp',['jpyHoliday.hpp',['../a00257.html',1,'']]]
 ];
--- docs/search/pages_5.js
+++ docs/search/pages_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['junified_20library_20integrating_20analytical_20and_20numerical_20techiniques',['jUnified Library Integrating Analytical and Numerical Techiniques',['../index.html',1,'']]]
+  ['junified_20library_20integrating_20analytical_20and_20numerical_20techniques',['jUnified Library Integrating Analytical and Numerical Techniques',['../index.html',1,'']]]
 ];
